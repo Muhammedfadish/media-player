6502bf226b86abb66a92899db53233a40fc8bf1d
Muhammedfadish <fadishfadi8@gmail.com>
2024-11-07 09:43:39 +0530

    mediaPlayer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,29 @@
 import React, { useState } from 'react'
-import Add from '../components/Add'
-import { Link } from 'react-router-dom'
-import View from '../components/View'
+import Add from '../Components/Add'
+import View from '../Components/View'
 import Category from '../components/Category'
+import { Link } from 'react-router-dom'
+
+
 
 const Home = () => {
-  const [addResponseFromHome,setAddResponseFromHome]=useState("")
-  const [deleteResponseFromCategory,setDeleteResponseFromCategory] = useState("")
- 
+  const [deleteResponseFromView,setdeleteResponseFromView] = useState("")
+  const [deleteResponseFromCategory,setdeleteResponseFromCategory] = useState("")
+  const [addResponse,setAddResponse] = useState("")
   return (
     <div style={{paddingTop:'100px'}}>
       <div className="container mb-5 d-flex justify-content-between">
-        <Add setAddResponseFromHome={setAddResponseFromHome}/>
+        <Add setAddResponse={setAddResponse}/>
         <Link to={'/history'}>Watch History</Link>
       </div>
       <div className="container-fluid row my-5">
-        <div className='col-lg-6'>
-          <h3>All Videos</h3>
-          <View deleteResponseFromCategory={setDeleteResponseFromCategory} addResponseFromHome={addResponseFromHome}/>
-        </div>
         <div className="col-lg-6">
-          <Category setDeleteResponseFromCategory={setDeleteResponseFromCategory}/>
+          <h3>All Videos</h3>
+          <View setdeleteResponseFromView={setdeleteResponseFromView} deleteResponseFromCategory={deleteResponseFromCategory} addResponse={addResponse}/>
+        </div>
+
+        <div className="col-lg-6">
+          <Category deleteResponseFromView={deleteResponseFromView} setdeleteResponseFromCategory={setdeleteResponseFromCategory} />
         </div>
       </div>
     </div>
--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,3 +1,3 @@
-const SERVERURL ="http://localhost:3000"
+const SERVERURL ="https://mp-server-pe6c.onrender.com"
 
 export default SERVERURL
